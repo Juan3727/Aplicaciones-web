1e688e06de2e433332e2ac633521f5c6b0b16624
unknown <juanciss138@outlook.com>
2024-11-04 17:53:59 -0300

    first commit

--- components/card.js
+++ components/card.js
@@ -1,43 +1,72 @@
-import React, { useState } from 'react';
-
-export const CardElement = ({ titulo, descripcion, imagen, precio }) => {
-    const [cantidad, setCantidad] = useState(1);
-
-    const aumentarCantidad = () => {
-        setCantidad(cantidad + 1);
-    };
-
-    const disminuirCantidad = () => {
-        if (cantidad > 1) {
-            setCantidad(cantidad - 1);
-        }
-    };
-
-    return (
-        <div className="card">
-            <div className="header">
-                <h3>{titulo}</h3>
+export const cardElement = (titulo, imagen, descripcion, precio) => {
+    return `
+       <div class="card col-md-4">
+            <div class="card-header">
+                <div class="card-title">
+                    ${titulo}
+                </div>
             </div>
-            <div className="contenedor">
-                <div className="texto">
-                    <h3>{descripcion}</h3>
-                </div>
-                <img 
-                    src={imagen} 
-                    alt={descripcion} 
-                    height="200" 
-                    width="200" 
-                />
-                <div className="texto">
-                    <p>Precio: {precio}$</p>
-                </div>
-                <div className="cantidad">
-                    <button onClick={disminuirCantidad}>-</button>
-                    <span>{cantidad}</span>
-                    <button onClick={aumentarCantidad}>+</button>
-                </div>
-                <button className="boton2">Comprar o añadir al carrito</button>
+            <div class="card-body">
+                <img src="${imagen}" alt="${titulo}" height="200" width="200"/>
+                <p>${descripcion}</p>
+            </div>
+            <div class="card-price">
+                Precio: $${precio}
             </div>
         </div>
-    );
-};
+        <div class="card-button">
+                <button class="btn-rest">-</button>
+                <input type="number" value="1" min="1" class="quantity-input" />
+                <button class="btn-sum">+</button>
+            </div>
+            <button class="btn-add">Añadir al carrito</button>
+        </div>
+    `;
+}
+
+const productos = [
+    {
+        titulo: "Intel Core i7 14th Gen",
+        imagen: "../Elementos/Intelcorei714thgen.webp",
+        precio: 850000,
+        descripcion: "Procesador de Gama Media"
+    },
+    {
+        titulo: "Intel Core i9 12th Gen",
+        imagen: "../Elementos/Intelcorei912thgen.webp",
+        precio: 580000,
+        descripcion: "Procesador de Gama Alta"
+    },
+    {
+        titulo: "Memoria ram hyperx 8gb ddr4",
+        imagen: "../Elementos/Ramhyperx8gb.webp",
+        precio: 35000,
+        descripcion: "Memoria Ram"
+    },
+    {
+        titulo: "Memoria ram hyperx 16gb ddr4",
+        imagen: "../Elementos/Ramhyperx16gb.jpg",
+        precio: 57000,
+        descripcion: "Memoria Ram"
+    },
+    {
+        titulo: "Tarjeta grafica ASUS GEFORCE RTX 4070",
+        imagen: "../Elementos/AsusGeForceRTX4070.webp",
+        precio: 1100000,
+        descripcion: "Tarjeta Gráfica de Última Generación"
+    },
+    {
+        titulo: "Tarjeta grafica NVIDIA GEFORCE GTX 1050ti",
+        imagen: "../Elementos/NvidiaGeForce1050ti.jpg",
+        precio: 360000,
+        descripcion: "Tarjeta Gráfica Económica"
+    }
+];
+
+const cardsHTML = productos.map((product, index) => 
+    cardElement(product.titulo, product.imagen, product.descripcion, product.precio)
+).join('');
+
+document.getElementById('cards-container').innerHTML = cardsHTML;
+
+
--- components/index.js
+++ components/index.js
@@ -1,15 +1,17 @@
-const url = "";
+const url = "http://localhost:8000";
 
 const navElements = [
-  {title:'Rapigamer', link: `${url}home.html`},
-  {title:'Ingresar', link: `${url}login.html`},
-  {title:'Registrarse', link: `${url}registro.html`},
-  {title:'Procesadores', link: `${url}processors.html`},
-  {title:'Memorias Ram', link: `${url}ram.html`},
-  {title:'Tarjetas Gráficas', link: `${url}videocard.html`}
+  {title: 'Navdar', link: '../index.html'},
+  {title:'Rapigamer', link: '../public_page/home.html'},
+  {title:'Ingresar', link: '../public_page/login.html'},
+  {title: 'Icono', link: '../Elementos/computadora.png' },
+  {title:'Registrarse', link: '../public_page/registro.html'},
+  {title:'Procesadores', link: '../private_page/processors.html'},
+  {title:'Memorias Ram', link: '../private_page/ram.html'},
+  {title:'Tarjetas Gráficas', link: '../private_page/videocard.html'}
   ]
 
-  export const navBarComponent = `
+  const navBar = `
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -24,22 +26,21 @@ const navElements = [
               <li class="nav-item">
                 <a class="nav-link" href="${e.link}">${e.title}</a>
               </li>
-            `;
+              `
           }).join('')
         }
       </ul>
     </div>
   </div>
 </nav>
-`;
+`
 
 let navContainer = document.querySelector('header');
 let pageName = document.getElementById('pageName').value
 let title = document.getElementById('title')
-document.getElementById('navbar').innerHTML = navBarComponent;
-
 
 window.addEventListener('load', () => {
-  navContainer.innerHTML = navBar; 
+  navContainer.innerHTML = navBar
   title.textContent = `Bienvenido a ${pageName}`
-});
+  document.title = pageName
+})
